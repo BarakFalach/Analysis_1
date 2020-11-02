047f83533500a3eb40f1edfb66f343b09e03aa61
BarakFalach <barak8995@gmail.com>
2020-11-02 16:29:43 +0200

    supplier commit

--- src/Supplier.java
+++ src/Supplier.java
@@ -1,4 +1,43 @@
+import java.util.ArrayList;
+
 public class Supplier {
     private String id;
     private String name;
+    private ArrayList<Product> productList;
+
+    public Supplier(String _id, String _name){
+        id = _id;
+        name = _name;
+        productList = new ArrayList<>();
+    }
+    public void addProduct(Product newProduct)
+    {
+        productList.add(newProduct);
+    }
+    public void removeProduct(Product productToDelete)
+    {
+        productList.remove(productToDelete);
+    }
+    public int indexOfProduct(Product product)
+    {
+        return productList.indexOf(product);
+    }
+
+    public String toString(){
+        StringBuilder res = new StringBuilder(
+                "Supplier ID: " +
+                id.toString() +
+                " Supplier Name: " +
+                name + '\n' +
+                "Supplier Products:" + '\n');
+        for (Product product : productList) {
+            res.append(product.toString());
+            res.append('\n');
+        }
+        return res.toString();
+    }
+
+
+
+
 }
