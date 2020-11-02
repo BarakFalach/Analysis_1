a269944516f71770a966a8937300700fcbee2850
BarakFalach <barak8995@gmail.com>
2020-11-02 18:09:12 +0200

    supplier and product

--- src/Address.java
+++ src/Address.java
@@ -2,4 +2,31 @@ public class Address {
     public String city;
     public String street;
     public int number;
+
+    public Address(String city, String street, int number) {
+        this.city = city;
+        this.street = street;
+        this.number = number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", street='" + street + '\'' +
+                ", number=" + number +
+                '}';
+    }
 }
