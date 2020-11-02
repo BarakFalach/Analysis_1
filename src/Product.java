062967779ee50d07284282519b91a39551bdf1b8
BarakFalach <barak8995@gmail.com>
2020-11-02 16:39:56 +0200

    supplier and product

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,20 @@
 public class Product {
     private String id;
     private String name;
+    private Supplier mySupplier;
+
+    public Product(String _id, String _name)
+    {
+        id = _id;
+        name = _name;
+        mySupplier = null;
+    }
+    public void updateSupplier(Supplier newSupplier)
+    {
+        mySupplier = newSupplier;
+    }
+    public String toString()
+    {
+        return "Product ID: " + id.toString() +" Product Name: " + name;
+    }
 }
