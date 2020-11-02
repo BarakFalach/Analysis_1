b969d7b89cbea1a44560706b1c33e45727eb375d
asafsalo <3asafi51@gmail.com>
2020-11-02 16:40:54 +0200

    BTA

--- src/Web_User.java
+++ src/Web_User.java
@@ -3,17 +3,29 @@ public class Web_User {
     private String password;
     private UserState state;
 
-    public Web_User (String id, String pass, UserState userState){
+    public Web_User(String id, String pass, UserState userState) {
         this.login_id = id;
         this.password = pass;
         this.state = userState;
     }
 
-    public String getLogin_id(){
+    public String getLogin_id() {
         return login_id;
     }
 
-    public UserState getUserState(){
+    public String getPassword() {
+        return password;
+    }
+
+    public void logout() {
+        this.state = UserState.Blocked;
+    }
+
+    public void setUserState(UserState userState) {
+        this.state = userState;
+    }
+
+    public UserState getUserState() {
         return state;
     }
 }
--- src/main.java
+++ src/main.java
@@ -1,34 +1,39 @@
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Optional;
 
 public class main {
 
-    private ArrayList<Web_User> web_users = new ArrayList<Web_User>();
-    private ArrayList<Supplier> suppliers = new ArrayList<Supplier>();
-    private ArrayList<Product> products = new ArrayList<Product>();
+    private static ArrayList<Web_User> web_users = new ArrayList<Web_User>();
+    private static ArrayList<Supplier> suppliers = new ArrayList<Supplier>();
+    private static ArrayList<Product> products = new ArrayList<Product>();
 
     public static void main(String[] args) {
+
+
     }
 
-    public UserState add_WebUser(String id){
-        if(web_users.stream().anyMatch(user -> user.getLogin_id().equals(id))){
-            return UserState.Banned;
-        }
-
-        // Tom - insert password;
-        String password = "223";
-
-        Web_User newUser = new Web_User(id, password, UserState.New);
+    public UserState add_WebUser(String id, String password){
+        web_users.add(new Web_User(id, password, UserState.New));
         return UserState.New;
     }
 
-    public void remove_WebUser(String id){
-        if(web_users.stream().anyMatch(user -> user.getLogin_id().equals(id)))
-            web_users.removeIf(o->o.getLogin_id().equals(id));
+    public boolean remove_WebUser(String id){
+        return web_users.removeIf(o -> o.getLogin_id().equals(id));
     }
 
-    public void login_WebUser(int id){
-
+    public UserState login_WebUser(String id, String password){
+        if(web_users.stream().anyMatch(o -> o.getLogin_id().equals(id) && o.getPassword().equals(password))) {
+            web_users.stream().filter(o -> o.getLogin_id().equals(id)).findFirst().get().setUserState(UserState.Active);
+        }
+        return UserState.Active;
     }
 
+    public UserState logoutUser(String id){
+        if(web_users.stream().anyMatch(o -> o.getLogin_id().equals(id))) {
+            web_users.stream().filter(o -> o.getLogin_id().equals(id)).findFirst().get().setUserState(UserState.Blocked);
+        }
+        return UserState.Blocked;
+    }
 
 }
